032f58e117d99f1df072608e3d6da20ce6b0fba4
Yu Hao <yuhao600@gmail.com>
2014-12-08 20:51:47 +0800

    14 in Ruby

---
+++ src/014. Longest Collatz sequence/014.rb
@@ -0,0 +1,23 @@
+def collatz(num)
+  i = 1
+  while num != 1
+    i += 1
+    num = case num.even?
+	      when true  then num / 2
+		  when false then num * 3 + 1
+		  end
+  end
+  return i
+end
+
+max_num = 0
+max_collatz = 0
+1.upto(1000000) do |num|
+  result = collatz(num)
+  if result > max_collatz
+    max_collatz = result
+	max_num = num
+  end
+end
+
+puts max_num
