3d38958921c661e9017552fa428aa9caf3919333
Yu Hao <yuhao600@gmail.com>
2014-12-08 23:09:06 +0800

    30 in Ruby

---
+++ src/030. Digit fifth powers/030.rb
@@ -0,0 +1,1 @@
+def sum_of_fifth_power(num)  sum = 0  while num > 0    sum += (num % 10) ** 5	num /= 10  end  sumendresult = []2.upto(999999) do |num|  result << num if sum_of_fifth_power(num) == numendputs result.inject(:+)
