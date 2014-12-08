0f25f0e0df60f9b7eef3111d9db0fbb483b873ed
Yu Hao <yuhao600@gmail.com>
2014-12-08 22:31:38 +0800

    21 in Ruby

---
+++ src/021. Amicable numbers/021.rb
@@ -0,0 +1,1 @@
+def d(num)  divisors = []  1.upto(Integer(Math.sqrt(num))) do |n|    if num % n == 0	  divisors << n	  if num / n != n and n != 1	    divisors << num / n	  end	end  end  divisors.inject(:+)endamicable = []1.upto(10000) do |num|  amicable << num if d(d(num)) == num and d(num) != numendp amicableputs amicable.inject(:+)
