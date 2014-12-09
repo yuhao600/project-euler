5b61c848b3c4fd973593a0e5ef2bab6f6965a8be
Yu Hao <yuhao600@gmail.com>
2014-12-09 20:41:20 +0800

    2 in C

---
+++ src/002. Even Fibonacci numbers/002.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned long long f1 = 1, f2 = 1;
+	unsigned long long sum = 0;
+	while (f1 < 4000000ULL)
+	{
+	    unsigned long long temp = f2;
+		f2 = f1 + f2;
+		f1 = temp;
+		if (f1 % 2 == 0)
+		{
+		    sum += f1;
+		}
+	}
+	printf("%llu\n", sum);
+	return 0;
+}
+
+int isprime(int num)
+{
+    if (num <= 1)
+	{
+	    return 0;
+	}
+	else if (num <= 3)
+	{
+	    return 1;
+	}
+	
