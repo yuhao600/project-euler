37d0fe140f4ea83ba65ed776d36ccd5da42a36cb
Yu Hao <yuhao600@gmail.com>
2014-12-09 20:40:44 +0800

    50 in Ruby

---
+++ src/050. Consecutive prime sum/050.rb
@@ -0,0 +1,18 @@
+require 'prime'
+
+sum = 0
+max = 0
+Prime.each do |prime|
+  sum += prime
+  result = sum
+  Prime.each(prime) do |small|
+    if Prime.prime? result
+      max = result if result > max and result < 1000000
+	  break
+    end
+	result -= small
+  end
+  break if result > 1000000
+end
+
+puts max
