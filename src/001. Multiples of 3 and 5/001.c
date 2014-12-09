1ffec540fac78b04c64ca470f0a829e009c0c7d6
Yu Hao <yuhao600@gmail.com>
2014-12-09 20:41:03 +0800

    1 in C

---
+++ src/001. Multiples of 3 and 5/001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sum = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+	return 0;
+}
