25a9298c44ceabda3101a7fe4575c91225681e94
Yu Hao <yuhao600@gmail.com>
2014-12-09 20:13:38 +0800

    41 in Ruby

---
+++ src/041. Pandigital prime/041.rb
@@ -0,0 +1,13 @@
+require 'prime'
+pandigital = []
+9.downto(1) do |digit|
+  (1 .. digit).to_a.permutation(digit).each do |arr|
+    num = 0
+    arr.each do |d|
+	  num = num * 10 + d
+	end
+	pandigital << num if Prime.prime? num
+  end
+end
+
+puts pandigital.max
