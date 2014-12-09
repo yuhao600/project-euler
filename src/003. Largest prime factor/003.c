576c948292aeaccea6b2a80c3ce765b469b4c253
Yu Hao <yuhao600@gmail.com>
2014-12-09 20:41:38 +0800

    3 in C

---
+++ src/003. Largest prime factor/003.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned long long num = 600851475143;
+	unsigned long long max = 2;
+	while (num % 2 == 0)
+		num /= 2;
+	unsigned long long factor = 3;
+	while (num > 1)
+	{
+		while (num % factor == 0)
+	    {
+			num /= factor;
+			max = factor;
+		}
+		factor += 2;
+	}
+	printf("%llu\n", max);
+	return 0;
+}
