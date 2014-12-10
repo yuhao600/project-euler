d19666fda1662e997abc456926d703e0c7a3bb51
Yu Hao <yuhao600@gmail.com>
2014-12-10 22:48:56 +0800

    Readme Generator

---
+++ script/readme_generator.rb
@@ -0,0 +1,52 @@
+class Problem
+  include Comparable
+  attr_reader :index, :title, :languages, :directory
+  
+  def initialize(index, title, directory)
+    @index = index
+	@title = title
+	@languages = []
+	@directory = directory
+  end
+  
+  def <=>(other)
+    self.index <=> other.index
+  end
+end
+
+problems = []
+Dir.foreach('../src') do |d|
+  if /(?<index>\d{3})\.\s*(?<title>\S.+)/ =~ d
+    p = Problem.new(index.to_i, title, d)
+	Dir.foreach("../src/#{d}") do |src|
+	  if /\d{3}\.(?<lang>.+)/ =~ src
+	    case lang
+		  when 'c'   then p.languages << :c
+		  when 'rb'  then  p.languages << :ruby
+		  when 'lua' then  p.languages << :lua
+		end
+	  end
+	end
+	problems << p
+  end
+end
+
+problems.sort!
+File.open('../Readme.md', 'w') do |readme|
+  readme.puts 'My Project Euler Solutions'
+  readme.puts '========'
+  readme.puts '|Index|Title|Solution|'
+  readme.puts '| ----- | -------- | -------- | ---------- |'
+  problems.each do |p|
+    link = ''
+    p.languages.each do |lang|
+      case lang
+	    when :c    then link += "[C](/src/#{p.directory}/#{sprintf('%03d', p.index)}.c) "
+	    when :cpp  then link += "[Ruby](/src/#{p.directory}/#{sprintf('%03d', p.index)}.cpp) "
+	    when :ruby then link += "[Ruby](/src/#{p.directory}/#{sprintf('%03d', p.index)}.rb) "
+	    when :lua  then link += "[Ruby](/src/#{p.directory}/#{sprintf('%03d', p.index)}.lua) "
+	  end
+    end
+  readme.puts "|#{p.index}|[#{p.title}](https://projecteuler.net/problem=#{p.index})|#{link}|"
+  end
+end
