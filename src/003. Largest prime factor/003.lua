f77ca8abc8b710b64cf353421ab3c0cc51da2594
Yu Hao <yuhao600@gmail.com>
2014-12-11 21:21:16 +0800

    3 in Lua

---
+++ src/003. Largest prime factor/003.lua
@@ -0,0 +1,18 @@
+local num = 600851475143
+local max = 0
+while num % 2 == 0 do
+  num = num / 2
+  max = 2
+end
+
+for f = 3, num, 2 do
+  while num % f == 0 do
+    num = num / f
+	max = f
+  end
+  if num == 1 then
+    break
+  end
+end
+
+print(max)
