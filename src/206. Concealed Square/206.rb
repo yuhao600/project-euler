63c2783cb8c6a078a76039c1721d8db8b1d98c7a
Yu Hao <yuhao600@gmail.com>
2014-12-13 02:19:30 +0800

    Improve performance for 206 in Ruby

--- src/206. Concealed Square/206.rb
+++ src/206. Concealed Square/206.rb
@@ -1,6 +1,8 @@
-Math.sqrt(10203040506070809).to_i.upto(Math.sqrt(20203040506070809).to_i) do |num|
-  if (num * num).to_s =~ /1\d2\d3\d4\d5\d6\d7\d8\d9/
-    puts num * 10
-	break
+Math.sqrt(10203040506070809).to_i.upto(Math.sqrt(19293949596979899).to_i) do |num|
+  if num % 10 == 3 or num % 10 == 7
+    if (num * num).to_s =~ /1\d2\d3\d4\d5\d6\d7\d8\d9/
+      puts num * 10
+	  break
+    end
   end
 end
