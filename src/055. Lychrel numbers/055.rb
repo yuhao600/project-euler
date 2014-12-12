6e6fd40710840f564cfcc98de4eebd3f0a56ed45
Yu Hao <yuhao600@gmail.com>
2014-12-13 00:19:44 +0800

    55 in Ruby

---
+++ src/055. Lychrel numbers/055.rb
@@ -0,0 +1,15 @@
+def isLychrel(num)
+  i = 0
+  while i < 50
+    num = num + num.to_s.reverse.to_i
+	if num == num.to_s.reverse.to_i
+	  return false
+	end
+	i += 1
+  end
+  return true
+end
+
+arr = []
+1.upto(10000) { |num| arr << num if isLychrel(num)}
+puts arr.length
