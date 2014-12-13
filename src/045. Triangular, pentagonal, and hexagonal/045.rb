fd72707b948e43f44ecc2f8a7d4e08e716b3bd82
Yu Hao <yuhao600@gmail.com>
2014-12-13 13:13:25 +0800

    45 in Ruby

---
+++ src/045. Triangular, pentagonal, and hexagonal/045.rb
@@ -0,0 +1,1 @@
+triangle, pentagonal, hexagonal = [], [], []1.upto(100000) do |n|  triangle << n * (n + 1) / 2  pentagonal << n * (3 * n - 1) / 2  hexagonal << n * (2 * n - 1)endputs (triangle & pentagonal & hexagonal).keep_if {|num| num > 40755}.min
