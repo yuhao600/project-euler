dedbb839bc9c69ad62260de6be24eb4ce181e468
Yu Hao <yuhao600@gmail.com>
2015-05-03 11:08:25 +0800

    97 in Java

---
+++ src/097. Large non-Mersenne prime/097.java
@@ -0,0 +1,15 @@
+import java.math.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		// 10 ^ 10
+		final BigInteger ten2Ten = BigInteger.valueOf(10).pow(10);
+		BigInteger powerTwo = BigInteger.valueOf(2).modPow(BigInteger.valueOf(7830457), ten2Ten);
+		BigInteger prime = powerTwo.multiply(BigInteger.valueOf(28433)).add(BigInteger.ONE);
+		BigInteger result = prime.mod(ten2Ten);
+		System.out.println(result);
+	}
+}
+
