0043d5f8eb1732f930ff6c4e65da13e328e68e50
Yu Hao <yuhao600@gmail.com>
2015-05-05 23:03:12 +0800

    16 in Java

---
+++ src/016. Power digit sum/016.java
@@ -0,0 +1,16 @@
+import java.math.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BigInteger power = BigInteger.valueOf(2).pow(1000);
+        byte[] arr = power.toString().getBytes();
+        int sum = 0;
+        for (byte digit: arr)
+        {
+            sum += digit - '0';
+        }
+        System.out.println(sum);
+    }
+}
