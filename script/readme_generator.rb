79f39387ce69179831fa7c002359739ec3e3675e
Yu Hao <yuhao600@gmail.com>
2015-05-18 23:41:35 +0800

    extract language extensions to a hash.

--- script/readme_generator.rb
+++ script/readme_generator.rb
@@ -4,9 +4,9 @@ class Problem
   
   def initialize(index, title, directory)
     @index = index
-	@title = title
-	@languages = []
-	@directory = directory
+    @title = title
+    @languages = []
+    @directory = directory
   end
   
   def <=>(other)
@@ -14,23 +14,25 @@ class Problem
   end
 end
 
+source_extensions = {
+  'c'    => :c,
+  'cpp'  => :cpp,
+  'java' => :java,
+  'lua'  => :lua,
+  'py'   => :python,
+  'rb'   => :ruby,
+}
+
 problems = []
 Dir.foreach('../src') do |d|
   if /(?<index>\d{3})\.\s*(?<title>\S.+)/ =~ d
     p = Problem.new(index.to_i, title, d)
-	Dir.foreach("../src/#{d}") do |src|
-	  if /\d{3}\.(?<lang>.+)/ =~ src
-	    case lang
-		  when 'c'    then p.languages << :c
-		  when 'cpp'  then p.languages << :cpp
-		  when 'java' then p.languages << :java
-		  when 'lua'  then p.languages << :lua
-		  when 'py'   then p.languages << :python
-		  when 'rb'   then p.languages << :ruby
-		end
-	  end
-	end
-	problems << p
+    Dir.foreach("../src/#{d}") do |src|
+      if /\d{3}\.(?<lang>.+)/ =~ src
+        p.languages << source_extensions[lang]
+      end
+    end
+    problems << p
   end
 end
 
@@ -45,13 +47,13 @@ File.open('../Readme.md', 'w') do |readme|
     link = ''
     p.languages.each do |lang|
       case lang
-	    when :c      then link += "[C](/src/#{p.directory}/#{sprintf('%03d', p.index)}.c) "
-	    when :cpp    then link += "[C++](/src/#{p.directory}/#{sprintf('%03d', p.index)}.cpp) "
-		when :java   then link += "[Java](/src/#{p.directory}/#{sprintf('%03d', p.index)}.java) "
-		when :lua    then link += "[Lua](/src/#{p.directory}/#{sprintf('%03d', p.index)}.lua) "
-		when :python then link += "[Python](/src/#{p.directory}/#{sprintf('%03d', p.index)}.py) "
-	    when :ruby   then link += "[Ruby](/src/#{p.directory}/#{sprintf('%03d', p.index)}.rb) "
-	  end
+        when :c      then link += "[C](/src/#{p.directory}/#{sprintf('%03d', p.index)}.c) "
+        when :cpp    then link += "[C++](/src/#{p.directory}/#{sprintf('%03d', p.index)}.cpp) "
+        when :java   then link += "[Java](/src/#{p.directory}/#{sprintf('%03d', p.index)}.java) "
+        when :lua    then link += "[Lua](/src/#{p.directory}/#{sprintf('%03d', p.index)}.lua) "
+        when :python then link += "[Python](/src/#{p.directory}/#{sprintf('%03d', p.index)}.py) "
+        when :ruby   then link += "[Ruby](/src/#{p.directory}/#{sprintf('%03d', p.index)}.rb) "
+      end
     end
   readme.puts "|#{p.index}|[#{p.title}](https://projecteuler.net/problem=#{p.index})|#{link}|"
   end
