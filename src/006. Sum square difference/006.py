9e173c2c43e55e1b3e335f006e37c48a35e19cc4
Yu Hao <yuhao600@gmail.com>
2015-05-21 22:48:33 +0800

    Update 6 in Python

--- src/006. Sum square difference/006.py
+++ src/006. Sum square difference/006.py
@@ -1,4 +1,4 @@
 sum_of_square = reduce(lambda x, y: x + y, map(lambda x: x * x, range(1, 101)))
 sqare_of_sum  = reduce(lambda x, y: x + y, range(1, 101))
 
-print(sqare_of_sum * sqare_of_sum - sum_of_square)
+print(sqare_of_sum ** 2 - sum_of_square)
