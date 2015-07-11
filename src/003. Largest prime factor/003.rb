a6489e7187c3e4549417dc54ff1af7c70c8a53a2
Yu Hao <yuhao600@gmail.com>
2015-07-11 19:41:16 +0800

    Use prime_division is simpler

--- src/003. Largest prime factor/003.rb
+++ src/003. Largest prime factor/003.rb
@@ -1,13 +1,3 @@
 require 'prime'
 
-num = 600851475143
-factors = []
-Prime.each do |prime|
-  while num % prime == 0
-    factors << prime
-    num /= prime
-  end
-  break if num == 1
-end
-
-puts factors.max
+puts 600851475143.prime_division[-1][0]
