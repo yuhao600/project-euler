776d4315c2602922a9b45fa9205f179f9fcc4ec2
Yu Hao <yuhao600@gmail.com>
2015-07-18 17:42:49 +0800

    1 in C++

---
+++ src/001. Multiples of 3 and 5/001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+	int sum = 0;
+	for (int i = 0; i < 1000; i++)
+    {
+	    if (i % 3 == 0 || i % 5 == 0)
+  	    {
+  	        sum += i;
+  	    }
+    }
+    std::cout << sum;
+}
