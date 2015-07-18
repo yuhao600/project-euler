07b9fa77427cdd834e85cd2390d69f8f361048ef
Yu Hao <yuhao600@gmail.com>
2015-07-18 17:58:25 +0800

    tabs to whitespaces continue

--- src/001. Multiples of 3 and 5/001.cpp
+++ src/001. Multiples of 3 and 5/001.cpp
@@ -6,9 +6,9 @@ int main(void)
     for (int i = 0; i < 1000; i++)
     {
         if (i % 3 == 0 || i % 5 == 0)
-  	    {
+        {
             sum += i;
-  	    }
+        }
     }
     std::cout << sum;
 }
