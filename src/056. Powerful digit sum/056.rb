fc2036cfe068d856a54084cfad968920d6806774
yuhao10 <yuhao10@baidu.com>
2016-09-26 16:47:52 +0800

    extract digit_sum method

--- src/056. Powerful digit sum/056.rb
+++ src/056. Powerful digit sum/056.rb
@@ -1,14 +1,21 @@
+class Integer
+  def digit_sum
+    n = self
+    digits = 0
+    while n > 0
+      digits += n % 10
+      n /= 10
+    end
+    return digits
+  end
+end
+
 max = 0
 1.upto(100) do |a|
   1.upto(100) do |b|
     exp = a ** b
-    digit_sum = 0
-    while exp > 0
-      digit_sum += exp % 10
-      exp /= 10
-    end
-    if digit_sum > max
-      max = digit_sum
+    if exp.digit_sum > max
+      max = exp.digit_sum
     end
   end
 end
