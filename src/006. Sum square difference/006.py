ec554d2d2a3f7023147af662cf5a6b0d721e6088
yuhao10 <yuhao10@baidu.com>
2016-09-30 14:29:17 +0800

    simplify 6 in Python

--- src/006. Sum square difference/006.py
+++ src/006. Sum square difference/006.py
@@ -1,6 +1,4 @@
-from functools import reduce
-
-sum_of_square = reduce(lambda x, y: x + y, map(lambda x: x * x, range(1, 101)))
-sqare_of_sum  = reduce(lambda x, y: x + y, range(1, 101))
+sum_of_square = sum([x * x for x in range(1, 101)])
+sqare_of_sum  = sum(range(1, 101))
 
 print(sqare_of_sum ** 2 - sum_of_square)
