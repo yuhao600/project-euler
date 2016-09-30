83e69b3548315761647c480d15639ba0966e5588
yuhao10 <yuhao10@baidu.com>
2016-09-30 15:33:15 +0800

    refactor code for 6 a little

--- src/006. Sum square difference/006.cpp
+++ src/006. Sum square difference/006.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    int sum100 = 100 * 101 / 2;
     int sum100pow = 0;
     for (int i = 1; i <= 100; ++i)
     {
-        sum100pow = sum100pow + i * i;
+        sum100pow += i * i;
     }
+    int sum100 = 100 * (100 + 1) / 2;
     std::cout << sum100 * sum100 - sum100pow << std::endl;
 }
--- src/006. Sum square difference/006.lua
+++ src/006. Sum square difference/006.lua
@@ -1,6 +1,6 @@
-sum100 = 100 * 101 / 2
 sum100pow = 0
 for i = 1, 100 do
   sum100pow = sum100pow + i * i
 end
+sum100 = 100 * (100 + 1) / 2
 print(sum100 * sum100 - sum100pow)
