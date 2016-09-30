72e8c16712c5574c498ab04f6b036040d2eefa92
yuhao10 <yuhao10@baidu.com>
2016-09-30 16:44:08 +0800

    simplify 20 in Ruby

--- src/020. Factorial digit sum/020.rb
+++ src/020. Factorial digit sum/020.rb
@@ -1,8 +1,1 @@
-sum = 0
-factorial = (1 .. 100).inject(:*)
-while factorial > 0
-  sum += factorial % 10
-  factorial /= 10
-end
-
-puts sum
+puts (1 .. 100).inject(:*).to_s.chars.map(&:to_i).inject(:+)
