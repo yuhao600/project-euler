6cbe32ed241ebbaa70baf7ecae2ab6880344af2f
yuhao10 <yuhao10@baidu.com>
2016-10-26 15:52:42 +0800

    99 in C

---
+++ src/099. Largest exponential/099.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define LINE_LEN 100
+
+int main()
+{
+    FILE *f = fopen("p099_base_exp.txt", "r");
+    char line[LINE_LEN];
+    double max_power = 0;
+    int max_line_no = 0;
+    int line_no = 0;
+    while (fgets(line, sizeof(line), f))
+    {
+        line_no += 1;
+        int base, exp;
+        sscanf(line, "%d,%d", &base, &exp);
+        double current_power = exp * log((double)base);
+        if (current_power > max_power)
+        {
+            max_power = current_power;
+            max_line_no = line_no;
+        }
+    }
+    printf("%d\n", max_line_no);
+}
