d6d184001eb4094ecbe1b078af7203e8fc8d56fd
yuhao10 <yuhao10@baidu.com>
2016-10-31 16:13:18 +0800

    move method to Integer class

--- src/080. Square root digital expansion/080.rb
+++ src/080. Square root digital expansion/080.rb
@@ -1,14 +1,16 @@
 require 'bigdecimal'
 require 'bigdecimal/math'
 
-def get_100_digits_sqrt(n)
-  sqrt = BigMath.sqrt(BigDecimal.new(n.to_s), 100).to_s
-  match = /\.(\d{100})/.match(sqrt)
-  if match
-    match[1].chars[0, 100].map(&:to_i).inject(:+)
-  else
-    0
+class Integer
+  def get_100_digits_sqrt
+    sqrt = BigMath.sqrt(BigDecimal.new(self.to_s), 100).to_s
+    match = /\.(\d{100})/.match(sqrt)
+    if match
+      match[1].chars.map(&:to_i).inject(:+)
+    else
+      0
+    end
   end
 end
 
-puts (1 .. 100).map {|n| get_100_digits_sqrt(n) }.inject(:+)
+puts (1 .. 100).map(&:get_100_digits_sqrt).inject(:+)
