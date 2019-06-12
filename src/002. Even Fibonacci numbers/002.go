70c6e9f28cc5e0b2bfe19c3e30493e78c00af318
Yu Hao <yuhao600@gmail.com>
2019-06-12 22:56:17 +0800

    2 in Go

---
+++ src/002. Even Fibonacci numbers/002.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main () {
+	f := make(map[int]int)
+	f[1] = 1
+	f[2] = 1
+
+	for i := 3; ; i++ {
+		f[i] = f[i - 2] + f[i - 1]
+		if f[i] > 4000000 {
+			break
+		}
+	}
+
+	sum := 0
+	for _, v := range(f) {
+		if v % 2 == 0 {
+			sum += v
+		}
+	}
+
+	fmt.Println(sum)
+}
