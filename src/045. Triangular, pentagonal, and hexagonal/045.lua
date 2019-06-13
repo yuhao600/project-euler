3c3a7ba6b1c6725a87f22c5127a2fe14160ef760
yuhao10 <yuhao10@baidu.com>
2019-06-13 11:22:36 +0800

    fix crlf problem in 045

--- src/045. Triangular, pentagonal, and hexagonal/045.lua
+++ src/045. Triangular, pentagonal, and hexagonal/045.lua
@@ -1,1 +1,21 @@
-local triangle, pentagonal, hexagonal = {}, {}, {}for n = 1, 100000 do  triangle[n * (n + 1) / 2] = true  pentagonal[n * (3 * n - 1) / 2] = true  hexagonal[n * (2 * n - 1)] = trueendlocal result = {}for v in pairs(triangle) do  if pentagonal[v] and hexagonal[v] then    table.insert(result, v)  endendtable.sort(result)for _, r in ipairs(result) do  if r > 40755 then    print(r)    break  endend
+local triangle, pentagonal, hexagonal = {}, {}, {}
+for n = 1, 100000 do
+  triangle[n * (n + 1) / 2] = true
+  pentagonal[n * (3 * n - 1) / 2] = true
+  hexagonal[n * (2 * n - 1)] = true
+end
+
+local result = {}
+for v in pairs(triangle) do
+  if pentagonal[v] and hexagonal[v] then
+    table.insert(result, v)
+  end
+end
+
+table.sort(result)
+for _, r in ipairs(result) do
+  if r > 40755 then
+    print(r)
+    break
+  end
+end
