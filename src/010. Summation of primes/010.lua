860a861229b3bc5e3743372354d813e1991c7479
yuhao10 <yuhao10@baidu.com>
2019-06-14 17:36:04 +0800

    extract generating primes to function in 10

--- src/010. Summation of primes/010.lua
+++ src/010. Summation of primes/010.lua
@@ -1,25 +1,28 @@
---Sieve of Eratosthenes
-local t = {}
-for i = 1, 2000000 do
-  t[i] = true
-end
-t[1] = false  -- 1 is not prime
-
-local factor = 2
-while factor <= math.sqrt(#t) do
-  for i = factor * 2, #t, factor do
-    if t[i] == true then
-      t[i] = false
-    end
+function gen_primes(n)
+  --Sieve of Eratosthenes
+  local sieve = {}
+  for i = 1, n do
+    sieve[i] = true
   end
-  repeat
-    factor = factor + 1
-  until t[factor] == true
+  sieve[1] = false  -- 1 is not prime
+  
+  local factor = 2
+  while factor <= math.sqrt(#sieve) do
+    for i = factor * 2, #sieve, factor do
+      sieve[i] = false
+    end
+    repeat
+      factor = factor + 1
+    until sieve[factor] == true
+  end
+  return sieve
 end
 
+local n = 2000000
+primes = gen_primes(n)
 local sum = 0
-for i = 1, #t do
-  if t[i] then
+for i = 1, n do
+  if primes[i] then
     sum = sum + i
   end
 end
