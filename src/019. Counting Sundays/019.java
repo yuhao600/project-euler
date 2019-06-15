9d0de97fd6a7273295dce18a8762f1c5f1b6a6f5
yuhao10 <yuhao10@baidu.com>
2019-06-15 20:18:01 +0800

    use LocalDate to rewrite 19 in Java

--- src/019. Counting Sundays/019.java
+++ src/019. Counting Sundays/019.java
@@ -1,18 +1,14 @@
-import java.util.*;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        GregorianCalendar firstOfMonth = new GregorianCalendar(1901, 1, 1);
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+public class Main {
+    public static void main(String[] args) {
+        LocalDate firstOfMonth = LocalDate.of(1901, 1, 1);
         int count = 0;
-        while (firstOfMonth.before(new GregorianCalendar(2000, 12, 31)))
-        {
-            if (firstOfMonth.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
-            {
+        while (firstOfMonth.isBefore(LocalDate.of(2000, 12, 31))) {
+            if (firstOfMonth.getDayOfWeek() == DayOfWeek.SUNDAY) {
                 count += 1;
             }
-            firstOfMonth.add(Calendar.MONTH, 1);
+            firstOfMonth = firstOfMonth.plusMonths(1);
         }
         System.out.println(count);
     }
