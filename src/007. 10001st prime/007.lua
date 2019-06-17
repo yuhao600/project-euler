8dc8d6e35ecbbd5a3705b75c01e5e52f57b8d440
yuhao10 <yuhao10@baidu.com>
2019-06-17 13:36:10 +0800

    refine sieve of Eratosthenes algorithm

--- src/007. 10001st prime/007.lua
+++ src/007. 10001st prime/007.lua
@@ -2,17 +2,22 @@ function gen_primes(n)
   --Sieve of Eratosthenes
   local sieve = {}
   for i = 1, n do
-    sieve[i] = true
+    if i % 2 == 0 then
+      sieve[i] = false
+    else
+      sieve[i] = true
+    end
   end
   sieve[1] = false  -- 1 is not prime
+  sieve[2] = true -- 2 is the only even prime
   
-  local factor = 2
+  local factor = 3
   while factor <= math.sqrt(#sieve) do
-    for i = factor * 2, #sieve, factor do
+    for i = factor * factor, #sieve, factor do
       sieve[i] = false
     end
     repeat
-      factor = factor + 1
+      factor = factor + 2
     until sieve[factor] == true
   end
   return sieve
--- src/010. Summation of primes/010.go
+++ src/010. Summation of primes/010.go
@@ -21,18 +21,23 @@ func genPrimes(n int) map[int]bool {
 	// Sieve of Eratosthenes
 	sieve := make(map[int]bool)
 	for i := 1; i < n; i++ {
-		sieve[i] = true
+		if i % 2 == 0 {
+			sieve[i] = false
+		} else {
+			sieve[i] = true
+		}
 	}
 	sieve[1] = false // 1 is not prime
+	sieve[2] = true // 2 is the only even prime
 	
-	factor := 2
+	factor := 3
 	for factor <= int(math.Sqrt(float64(n))) {
-		for i:= factor * 2; i < n; i += factor {
+		for i := factor * factor; i < n; i += factor {
 			sieve[i] = false
 		}
-		factor++
+		factor += 2
 		for sieve[factor] == false {
-			factor++
+			factor += 2
 		}
 	}
 	return sieve
