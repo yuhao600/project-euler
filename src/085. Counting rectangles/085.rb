0e35e4f9dbf2c9a8be261ac8dd855a9b5e3eda0b
yuhao10 <yuhao10@baidu.com>
2019-06-20 17:56:49 +0800

    refine 85 in Ruby

--- src/085. Counting rectangles/085.rb
+++ src/085. Counting rectangles/085.rb
@@ -1,11 +1,14 @@
-candidates = []
+nearest = [0, 0]
+nearest_diff = 2_000_000 * 2_000_000
 1.upto(2000) do |m|
   m.upto(2000) do |n|
     rectangle_num = m * n * (m + 1) * (n + 1) / 4
-    candidates << [rectangle_num, m, n]
-    break if rectangle_num > 2_000_000
+    diff = (rectangle_num - 2_000_000).abs
+    if diff < nearest_diff
+      nearest = [m, n]
+      nearest_diff = diff
+    end
   end
 end
 
-result = candidates.min_by { |r| (r[0] - 2_000_000).abs }
-puts result[1] * result[2]
+puts nearest[0] * nearest[1]
