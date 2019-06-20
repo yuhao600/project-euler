b675644d3e2aa96bfbdd03044cf7de9cf73774bf
yuhao10 <yuhao10@baidu.com>
2019-06-20 21:08:25 +0800

    18 & 67 in Ruby

---
+++ src/018. Maximum path sum I/018.rb
@@ -0,0 +1,15 @@
+file_content = IO.read('018.txt')
+
+triangle = []
+file_content.lines do |line|
+  triangle << line.split.map(&:to_i)
+end
+
+1.upto(triangle.size - 1) do |row|
+  triangle[row - 1] << 0
+  0.upto(triangle[row].size - 1) do |col|
+    triangle[row][col] += [triangle[row - 1][col], triangle[row - 1][col - 1]].max
+  end
+end
+
+puts triangle.last.max
