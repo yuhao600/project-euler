df594a86ba3830bcedf1ad7c972e3e0dc5b12c38
Yu Hao <yuhao600@gmail.com>
2019-07-03 01:44:20 +0800

    tune the code a little for 146 in Ruby

--- src/146. Investigating a Prime Pattern/146.rb
+++ src/146. Investigating a Prime Pattern/146.rb
@@ -3,27 +3,38 @@ require 'prime'
 upper_limit = 150_000_000
 $primes = []
 Prime.each(upper_limit + 1) {|p| $primes << p}
+$primes.shift # 2 is not needed
 
 def if_prime_pattern(n)
   return false if n % 3 == 0
   return false if n % 7 == 0
   return false if n % 13 == 0
+
+  # (n^2 + 13) % 7 == 0
+  return false if n % 7 == 1 or n % 7 == 6
+  # (n^2 + 3) % 7 == 0
+  return false if n % 7 == 2 or n % 7 == 5
+  # (n^2 + 9) % 13 == 0
+  return false if n % 13 == 2 or n % 13 == 11
+  # (n^2 + 1) % 13 == 0
+  return false if n % 13 == 5 or n % 13 == 8
+  # (n^2 + 3) % 13 == 0
+  return false if n % 13 == 6 or n % 13 == 7
+
   $primes.each do |p|
     break if p > n + 1
-    return false if (n * n + 1) % p == 0
-    return false if (n * n + 3) % p == 0
-    return false if (n * n + 7) % p == 0
-    return false if (n * n + 9) % p == 0
-    return false if (n * n + 13) % p == 0
-    return false if (n * n + 27) % p == 0
+    [1, 3, 7, 9, 13, 27].each do |a|
+      return false if (n * n + a) % p == 0
+    end
   end
+
   # tests above primes are consecutive
-  [5, 11, 15, 17, 19, 21, 23, 25].each do |a| 
+  [5, 11, 15, 17, 19, 21, 23, 25].each do |b|
     $primes.each do |p|
       # tests up to the biggest prime less than the square root of n * n + 25
       # the condition p > n + 1 works for 10, and certainly works for big enough n
       return false if p > n + 1
-      break if (n * n + a) % p == 0
+      break if (n * n + b) % p == 0
     end
   end
   true
