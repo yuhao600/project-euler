5c5bf74ead6196094c58856bb9ac025ec84d47a2
yuhao600 <yuhao600@gmail.com>
2019-07-27 05:07:39 +0800

    optimize algorithm of 504 with Pick's theorem and symmetry

--- src/504. Square on the Inside/504.rb
+++ src/504. Square on the Inside/504.rb
@@ -1,41 +1,63 @@
 require 'set'
 
-def make_key(m, n)
-  m << 16 | n
+m = 100
+
+def tri_key(x, y)
+  x, y = y, x if x > y
+  x << 8 | y
 end
 
-$cached_lattice_count = {}
-def lattice_count(m, n)
-  k = make_key(m, n)
-  v = $cached_lattice_count[k]
+$gcd = {}
+1.upto(m) do |x|
+  x.upto(m) do |y|
+    $gcd[tri_key(x, y)] = x.gcd(y)
+  end
+end
+
+def gcd(x, y)
+  $gcd[tri_key(x, y)]
+end
+
+$tri_cache = {}
+# https://en.wikipedia.org/wiki/Pick%27s_theorem
+def tri(x, y)
+  k = tri_key(x, y)
+  v = $tri_cache[k]
   return v if v != nil
-  c = ((m - 1) * (n - 1) - m.gcd(n) + 1) / 2
-  $cached_lattice_count[k] = c
-  c
+  points = ((x - 1) * (y - 1) - gcd(x, y) + 1) / 2
+  $tri_cache[k] = points
+  points
 end
 
-upper_limit = 100
+def quad_key(a, b, c, d)
+  key_arr = tri_key(a, b), tri_key(b, c), tri_key(c, d), tri_key(d, a)
+  key_arr.sort!
+  key_arr.join('_').to_sym
+end
+
+def lattice_count(a, b, c, d) 
+  tri(a, b) + tri(b, c) + tri(c, d) + tri(d, a) + a + b + c + d - 3
+end
 
 $squares = Set.new
-1.upto(upper_limit * 1.5) do |p|
+1.upto(m * 2) do |p|
   $squares << p * p
 end
 
-def if_square(a, b, c, d)
-  total = lattice_count(a, b) + lattice_count(b, c) + lattice_count(c, d) + lattice_count(d, a) + (a - 1) + (b - 1) + (c - 1) + (d - 1) + 1
-  $squares.include? total
-end
-
-# brute force
-square_lattice_count = 0
-1.upto(upper_limit) do |a|
-  1.upto(upper_limit) do |b|
-    1.upto(upper_limit) do |c|
-      1.upto(upper_limit) do |d|
-        square_lattice_count += 1 if if_square(a, b, c, d)
+count = 0
+1.upto(m) do |a|
+  1.upto(m) do |b|
+    a.upto(m) do |c|
+      b.upto(m) do |d|
+        if $squares.include? lattice_count(a, b, c, d)
+          count += 1
+          count += 1 unless a == c
+          count += 1 unless b == d
+          count += 1 unless a == c or b == d
+        end
       end
     end
   end
 end
 
-puts square_lattice_count
+puts count
