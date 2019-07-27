9bbbd40b4d497b479f064e36cecaf1b16279295f
yuhao600 <yuhao600.gmail.com>
2019-07-27 14:33:23 +0800

    145 in Lua

---
+++ src/145. How many reversible numbers are there below one-billion/145.lua
@@ -0,0 +1,24 @@
+function reversible_count(d)
+  if d % 4 == 1 then
+    return 0
+  end
+  -- 2 digits: no carry over, and sum of two digits is odd
+  -- 2 digits => 20
+  -- 4 digits: outer pair is the same as 2 digits
+  --           inner pair has 30 possibilities because 0 is allowed
+  if d % 4 == 0 or d % 4 == 2 then
+    return 20 * math.pow(30, d / 2 - 1)
+  end
+  -- the first and third digit must be odd and gives carry over: 20 possiblilities
+  -- middle digit: add to itself, no carry over: 5 possibilities
+  -- 4 digits: same as before, no leading zero		
+  if d % 4 == 3 then
+    return 100 * math.pow(500, (d - 3) / 4)
+  end
+end
+
+local count = 0
+for d = 1, 9 do
+  count = count + reversible_count(d)
+end
+print(string.format("%d", count))
