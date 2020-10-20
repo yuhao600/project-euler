e26b092e6e52b2f3a326038ccd850fbe79f2f62f
yuhao10 <yuhao10@baidu.com>
2020-10-20 19:54:35 +0800

    rewrite 1 with stream

--- src/001. Multiples of 3 and 5/001.java
+++ src/001. Multiples of 3 and 5/001.java
@@ -1,11 +1,10 @@
+import java.util.stream.IntStream;
 public class Main {
+
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                sum += i;
-            }
-        }
+        int sum = IntStream.range(1, 1000)
+                .filter(n -> n % 3 == 0 || n % 5 == 0)
+                .sum();
         System.out.println(sum);
     }
 }
